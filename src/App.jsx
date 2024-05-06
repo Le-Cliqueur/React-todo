930c4b374a06b5926013484c06734ae6639ee7e9
Paul Schmitt <paul.schmitt456@gmail.com>
2024-05-06 20:24:16 +0200

    v1 pas terrible

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
+import Todo from './Todo'
 
 function App() {
+  const [todos, setTodos] = useState(["hello", "hel"])
   const [count, setCount] = useState(0)
+  const [value, setValue] = useState('')
 
   function addCount() {
     setCount(count + 1);
+  }
+
+  function updateTodo() {
+    setTodos([...todos, value]);
+    setCount(count + 1);
+    setValue('');
+  }
+
+
+  function updateValue(e) {
+    const input = e.target.value;
+    setValue(input);
   }
 
   return (
@@ -12,6 +27,15 @@ function App() {
       <p>C'est moi</p>
       <h1>{count}</h1>
       <button onClick={() => addCount()}>Click here</button>
+
+      <input type='text' value={value} className='inp' onChange={(e) => updateValue(e)}></input>
+      <button onClick={(e) => updateTodo(e)}>ADD</button>
+      <Todo/>
+      <ul>
+        {todos.map(item => (
+          <li>{item}</li>
+        ))}
+      </ul>
     </>
   )
 }
---
+++ src/Todo.jsx
@@ -0,0 +1,12 @@
+function Todo() {
+
+    return (
+        <div>
+            <h1>s</h1>
+            <p>so</p>
+            <p>so</p>
+        </div>
+    )
+}
+
+export default Todo;
